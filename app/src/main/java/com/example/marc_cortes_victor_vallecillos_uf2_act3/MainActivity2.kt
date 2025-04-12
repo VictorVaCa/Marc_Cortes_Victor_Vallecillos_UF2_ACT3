d099685671d6276979ed639987f52df6e08c824b
MarcCortes <201642353+fgdesgr@users.noreply.github.com>
2025-04-12 10:05:14 +0200

    yippiiiii

---
+++ app/src/main/java/com/example/marc_cortes_victor_vallecillos_uf2_act3/MainActivity2.kt
@@ -0,0 +1,28 @@
+package com.example.marc_cortes_victor_vallecillos_uf2_act3
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+
+        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(layout: MotionLayout?, currentId: Int) {
+                if (currentId == R.id.end) {
+                    val intent = Intent(this@SecondActivity, ThirdActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+        })
+    }
+}
---
+++ app/src/main/java/com/example/marc_cortes_victor_vallecillos_uf2_act3/MainActivity3.kt
@@ -0,0 +1,35 @@
+package com.example.marc_cortes_victor_vallecillos_uf2_act3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import android.widget.Button
+
+class ThirdActivity : AppCompatActivity() {
+    private var isLeft = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+
+        val layout = findViewById<ConstraintLayout>(R.id.thirdLayout)
+        val button = findViewById<Button>(R.id.movingButton)
+
+        button.setOnClickListener {
+            val constraintSet = ConstraintSet()
+            constraintSet.clone(layout)
+
+            if (isLeft) {
+                constraintSet.clear(R.id.movingButton, ConstraintSet.START)
+                constraintSet.connect(R.id.movingButton, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 16)
+            } else {
+                constraintSet.clear(R.id.movingButton, ConstraintSet.END)
+                constraintSet.connect(R.id.movingButton, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 16)
+            }
+
+            constraintSet.applyTo(layout)
+            isLeft = !isLeft
+        }
+    }
+}
